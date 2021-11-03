7e925cb11c4ff15203983b07248b8d8d7b0d2882
Dezx20 <2009awektoppo@gmail.com>
2021-11-03 23:38:36 +0530

    intro with express

---
+++ 3. Intro to express.js/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); //Allows the req to continue to the next middleware in line
+});
+app.use((req, res, next) => {
+  console.log("In the Another middleware");
+  res.send("<h1>hello from backend</h1>");
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+app.listen(3000);
