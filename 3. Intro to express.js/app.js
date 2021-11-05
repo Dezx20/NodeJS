08b5f247e8e741382886dcb6032f71abaaabd8ca
Dezx20 <2009awektoppo@gmail.com>
2021-11-05 22:46:20 +0530

    ..

--- 3. Intro to express.js/app.js
+++ 3. Intro to express.js/app.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("YohOO im here");
+// app.use("/", (req, res, next) => {
+//   console.log("YohOO im here");
 
-  next();
-});
+//   next();
+// });
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
   console.log("In the porduct middleware");
-  res.send("<h1>Products page</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add products</button></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log("add-product route");
+  console.log(req.body);
+
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
